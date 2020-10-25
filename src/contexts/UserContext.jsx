6169e44c39ba73faac91d2437d69c21f96822c96
Stepan <goodhrommi@gmail.com>
2020-10-25 21:56:37 +0300

    create UserContext

---
+++ src/contexts/UserContext.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import cookies from 'js-cookie';
+import faker from 'faker';
+
+const NICKNAME_COOCKIE = 'nickname';
+
+const UserContext = React.createContext({
+  nickname: null,
+});
+
+const UserProvider = ({ children }) => {
+  const nickname = cookies.get(NICKNAME_COOCKIE) || faker.internet.userName();
+  cookies.set(NICKNAME_COOCKIE, nickname);
+
+  return (
+    <UserContext.Provider value={{ nickname }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => {
+  const context = React.useContext(UserContext);
+  if (context === undefined) {
+    throw new Error("'useUser' must be within a 'UserProvider'");
+  }
+  return context;
+};
+
+export default UserProvider;
