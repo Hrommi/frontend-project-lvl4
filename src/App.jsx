f246b2ff1a29274f8d3d34db16660248bf731967
Stepan <goodhrommi@gmail.com>
2020-11-04 21:52:16 +0300

    update layout and use Messages component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import Channels from './features/channels';
+import Messages from './features/messages';
 
 const App = ({ channels }) => (
-  <Channels channels={channels} />
+  <Row className="h-100 pb-3">
+    <Col xs="3" className="border-right">
+      <Channels channels={channels} />
+    </Col>
+    <Col className="h-100">
+      <Messages />
+    </Col>
+  </Row>
 );
 
 export default App;
