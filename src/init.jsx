771e23cf2e4f4e880c174184eeae2409060b09c2
Stepan <goodhrommi@gmail.com>
2020-11-04 21:37:24 +0300

    add ToastProvider

--- src/init.jsx
+++ src/init.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import UserProvider from './contexts/UserContext';
 import App from './App';
+import { ToastProvider } from './components/Toast';
 
 export default (channels) => {
   render(
     <UserProvider>
-      <App channels={channels} />
+      <ToastProvider>
+        <App channels={channels} />
+      </ToastProvider>
     </UserProvider>,
     document.getElementById('chat'),
   );
