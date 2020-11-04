d746517e8364c4b4a6ee9fe1510cfb9d3d43d17e
Stepan <goodhrommi@gmail.com>
2020-11-04 21:49:51 +0300

    update initialization for messages feature

--- src/index.js
+++ src/index.js
@@ -2,13 +2,9 @@
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import gon from 'gon';
 
 import '../assets/application.scss';
-
-// import faker from 'faker';
-import gon from 'gon';
-// import cookies from 'js-cookie';
-// import io from 'socket.io-client';
 
 import run from './init';
 
@@ -16,6 +12,4 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-const { channels } = gon;
-
-run(channels);
+run(gon);
--- src/init.jsx
+++ src/init.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import io from 'socket.io-client';
 import UserProvider from './contexts/UserContext';
 import App from './App';
+import store from './store';
+import { setMessages, addMessage } from './features/messages';
 import { ToastProvider } from './components/Toast';
 
-export default (channels) => {
+export default ({ channels, messages }) => {
+  store.dispatch(setMessages({ messages }));
+
+  const socket = io();
+  socket.on('newMessage', ({ data }) => {
+    store.dispatch(addMessage({ message: data.attributes }));
+  });
+
   render(
     <UserProvider>
       <ToastProvider>
-        <App channels={channels} />
+        <Provider store={store}>
+          <App channels={channels} />
+        </Provider>
       </ToastProvider>
     </UserProvider>,
     document.getElementById('chat'),
